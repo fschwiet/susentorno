dcabbf3a867f782a7c8b9dea0baa747b36dab96a
fschwiet <fschwiet@gmail.com>
2026-08-06 13:42:41 -0500

    disable pyYAML install for now

--- templates/vm-shared-linux/pre-scripts/02-install-pnpm.sh
+++ templates/vm-shared-linux/pre-scripts/02-install-pnpm.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-pip install PyYAML # Used by codex to validate YAML
+# revisit this- use pipx?
+# Error was: error: externally-managed-environment
+#
+# pip install PyYAML # Used by codex to validate YAML
+
 
 curl -fsSL https://get.pnpm.io/install.sh | bash -
 
